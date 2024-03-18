a437a5a220b5e7745dd23ce37ad4aa99583e34d0
Adam Lesta <altairinglorious@proton.me>
2024-03-18 11:01:18 +0200

    ref: export webhook update type

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { createHash, createHmac } from "node:crypto";
 import type { CreateInvoiceProps } from "./interfaces/CreateInvoice";
 import type { CryptoBotConfig } from "./interfaces/CryptoBotConfig";
 import type { Invoice } from "./interfaces/Invoice";
+export type { WebhookUpdate } from "./interfaces/WebhookUpdate";
 
 export const CRYPTOBOT_MAIN_NETWORK = "https://pay.crypt.bot";
 export const CRYPTOBOT_TEST_NETWORK = "https://testnet-pay.crypt.bot";
